bfe57db74b50cb7210bd96881d1ac371b232ff71
David Furfero <furf@proof.xyz>
2023-01-11 10:18:32 -0800

    lots of cleanup

---
+++ constants.mjs
@@ -0,0 +1,13 @@
+// Directories
+export const ASSET_DIR = './assets';
+const TMP_DIR = '/tmp';
+
+export const SOURCE_DIR = `${ASSET_DIR}/source`;
+export const PROCESSING_DIR = `${TMP_DIR}/grails`;
+export const PROCESSED_DIR = `${ASSET_DIR}/source-processed`;
+export const CLEAN_DIR = `${ASSET_DIR}/clean`;
+
+export const EXIFTOOL_SUFFIX = '_original';
+
+// Config
+export const DEBUG_MODE = false;
